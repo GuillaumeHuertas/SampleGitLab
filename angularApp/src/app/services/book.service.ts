61a31167d1e8c36fc31c64a21a74a52e3a220952
Administrator <st.balaguer@gmail.com>
2019-09-12 13:32:43 +0000

    Update book.service.ts

--- angularApp/src/app/services/book.service.ts
+++ angularApp/src/app/services/book.service.ts
@@ -7,7 +7,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class BookService implements OnInit {
-  addr = "http://127.0.0.1:8080/api/v1/book"
+  addr = "http://192.168.206.27:8080/api/v1/book"
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
